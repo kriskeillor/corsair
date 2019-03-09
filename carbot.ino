c8690f153c869c9f5b707fec72bd61d979aeb8d2
Kristopher Keillor <keillok@wwu.edu>
2019-03-08 21:50:16 -0800

    Add memory to sns

--- carbot.ino
+++ carbot.ino
@@ -7,7 +7,7 @@
 Servo leftServo;
 Servo rightServo;
 
-int lastLeft, lastCent, lastRight;
+float leftMem, centMem, rightMem;
 
 void setup() {
   Serial.begin(9600);
@@ -21,9 +21,9 @@ void setup() {
   pinMode(CENT_SNS, INPUT);
   pinMode(RIGHT_SNS, INPUT);
 
-  lastLeft = 0;
-  lastCent = 0;
-  lastRight = 0;
+  leftMem = 0.0;
+  centMem = 0.0;
+  rightMem = 0.0;
 }
 
 void loop() {
@@ -31,76 +31,106 @@ void loop() {
   int centRead = digitalRead(CENT_SNS);
   int rightRead = digitalRead(RIGHT_SNS);
 
-  String leftSns = "Left Sns " + String(leftRead);
-  String centSns = ", Cent Sns " + String(centRead);
-  String rightSns = ", Right Sns " + String(rightRead);
-  Serial.print(leftSns + centSns + rightSns + "\n");
-
+  if (leftRead == 0 && centRead == 0 && rightRead == 0) 
+  {
+    // if we're off track, buff up the last sns data
+    if (leftMem > 0.25) leftRead = 1;
+    if (centMem > 0.75) centRead = 1;
+    if (rightMem > 0.25) rightRead = 1;
+  } else {
+    leftMem = (smoothstep(0, 1, leftMem) * 4 + (float)leftRead) / 5;
+    centMem = (smoothstep(0, 1, centMem) * 4 + (float)centRead) / 5;
+    rightMem = (smoothstep(0, 1, rightMem) * 4 + (float)rightRead) / 5;
+  }
+  
+  String snsRead = ("Readout " + String(leftRead) + String(centRead) + String(rightRead) + "; ");
+  
+  String leftMem = "Memory " + String(leftMem) + "/";
+  String centMem = String(centMem) + "/";
+  String rightMem = String(rightMem) + "; ";
+  String memMsg = leftMem + centMem + rightMem;
+  Serial.print(snsRead + memMsg);
+  
   if (leftRead == 0 && centRead == 0 && rightRead == 0) {
-      leftRead = lastLeft;
-      centRead = lastCent;
-      rightRead = lastRight;
-  }
-  if (leftRead == 0 && centRead == 1 && rightRead == 0) {
-    forward();
-  }
-   if (leftRead==1 && centRead==0 && rightRead==0) {
-    easyLeft();
+    // if we're STILL off track, reverse 
+    trackFind();
   }
 
-   if (leftRead==0 && centRead==0 && rightRead==1) {
+  // steering block follows 
+  if (leftRead == 0 && centRead == 1 && rightRead == 0) 
+    forward();
+  
+  if (leftRead==1 && centRead==0 && rightRead==0) 
+    easyLeft();
+
+  if (leftRead==0 && centRead==0 && rightRead==1) 
     easyRight();
-  }
-  if (leftRead == 1 && centRead == 1 && rightRead == 0) {
+    
+  if (leftRead == 1 && centRead == 1 && rightRead == 0) 
     sharpLeft();
-  }
   
   if (leftRead==0 && centRead==1 && rightRead==1)
     sharpRight();
 
-  if (leftRead==1 && centRead==0 && rightRead==1) {
-    leftRightBlack();
-  }
-  if (leftRead==1 && centRead==1 && rightRead==1) {
-    trackFind();
-  }
+  if (leftRead==1 && centRead==0 && rightRead==1) 
+    trackFindLeft();
+  
+  if (leftRead==1 && centRead==1 && rightRead==1) 
+    trackFindLeft();
 }
 
 void sharpLeft() {
-  leftServo.write(90);
-  rightServo.write(0);
-  Serial.print("Sharp left turn");
+  writeRevs(90, 0);
 }
 
 void easyLeft() {
-  leftServo.write(95);
-  rightServo.write(5);
-  Serial.print("Easy left turn");
+  float leftRevs = 95 + 10 * rightMem;
+  float rightRevs = 75 - 20 * leftMem;
+  writeRevs(leftRevs, rightRevs);
 }
 
 void forward() {
-  leftServo.write(180);
-  rightServo.write(0);
-  Serial.print("Straight forward, full speed");
+  float leftRevs = 100 + 20 * leftMem;
+  float rightRevs = 80 - 20 * rightMem;
+  writeRevs(leftRevs, rightRevs);
 }
 
 void sharpRight() {
-  leftServo.write(190);
-  rightServo.write(85);
-  Serial.print("Sharp Right turn");
+  writeRevs(180, 85);
 }
 void trackFind() {
-  leftServo.write(145);
-  rightServo.write(90);
-  Serial.print("Track Found");
+  writeRevs(95, 60);
 }
-void leftRightBlack() {
-  leftServo.write(80);
-  rightServo.write(100);
-  Serial.print("Wierd shits happening, slowing down");
+
+void trackFindLeft() {
+  writeRevs(135, 85);
+}
+void Reverse() {
+  writeRevs(80, 100);
 }
 void easyRight() {
-  leftServo.write(175);
-  rightServo.write(90);
-  Serial.print("Easy Right turn");
+  float leftRevs = 105 + 20 * rightMem;
+  float rightRevs = 85 - 10 * leftMem;
+  writeRevs(leftRevs, rightRevs);
+}
+
+void writeRevs(float L, float R) {
+  leftServo.write(L);
+  rightServo.write(R);
+  Serial.print("Revs L" + String(L) + " / R" + String(R) + "\n");
+}
+
+float smoothstep(float edge0, float edge1, float x) {
+  // Scale, bias and saturate x to 0..1 range
+  x = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0); 
+  // Evaluate polynomial
+  return x * x * (3 - 2 * x);
+}
+
+float clamp(float x, float lowerlimit, float upperlimit) {
+  if (x < lowerlimit)
+    x = lowerlimit;
+  if (x > upperlimit)
+    x = upperlimit;
+  return x;
 }
