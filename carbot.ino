1b4abb4a14f9a1820703bda8125cc92d35910e90
Kristopher Keillor <keillok@wwu.edu>
2019-03-08 18:40:37 -0800

    Better average for sns and slightly better steering

--- carbot.ino
+++ carbot.ino
@@ -29,7 +29,7 @@ void setup() {
 }
 
 void loop() {
-  float snsScaler = 0.9f;
+  float snsScaler = 0.975f;
   int leftRead = digitalRead(LEFT_SNS);
   int centRead = digitalRead(CENT_SNS);
   int rightRead = digitalRead(RIGHT_SNS);
@@ -45,31 +45,43 @@ void loop() {
     snsScaler = 1.0f;
   }
   else {
-    //average the last & current readout, weighted by 3 & 2, respectively 
-    leftSns = (leftSns * 3 + (float)leftRead * 2) / 5;
-    centSns = (centSns * 3 + (float)centRead * 2) / 5;
-    rightSns = (rightSns * 3 + (float)rightRead * 2) / 5;
+    //average the last & current readout
+    leftSns = (smoothstep(0, 1, leftSns) + (float)leftRead) / 2;
+    centSns = (smoothstep(0, 1, centSns) + (float)centRead) / 2;
+    rightSns = (smoothstep(0, 1, rightSns) + (float)rightRead) / 2;
     onTrack = true;
   }
 
-  String leftMem = "Memory " + String(leftSns);
-  String centMem = String(centSns);
+  String leftMem = "Memory " + String(leftSns) + "/";
+  String centMem = String(centSns) + "/";
   String rightMem = String(rightSns) + "; ";
   String memMsg = leftMem + centMem + rightMem;
 
   String revMsg = "";
-  // rev speed is proportional to last center sns readout  
-  float revs = 5 + 40 * centSns;
 
   // basic idea is we get half throttle
-  float leftScale = (centSns * 2 + rightSns - leftSns) / 4;
+  float leftScale, rightScale;
+  if (leftSns > rightSns) {
+    leftScale = (centSns * 3 - rightSns) / 4;
+    rightScale = (centSns * 3 + leftSns) / 4;
+  }
+  else {
+    leftScale = (centSns * 3 + rightSns) / 4;
+    rightScale = (centSns * 3 - leftSns) / 4;
+  }
+/*  float leftScale = (centSns * 4 + rightSns * 3 - leftSns) / 8;
   float leftRev = 90 + 90 * smoothstep(0, 1, leftScale);
-  float rightScale = (centSns * 2 - rightSns + leftSns) / 4;
+  float rightScale = (centSns * 4 - rightSns + leftSns * 3) / 8;
+  float rightRev = 90 - 90 * smoothstep(0, 1, rightScale);*/
+  
+  //float leftScale = (centSns * 4 + rightSns * 3 - leftSns) / 8;
+  float leftRev = 90 + 90 * smoothstep(0, 1, leftScale);
+  //float rightScale = (centSns * 4 - rightSns + leftSns * 3) / 8;
   float rightRev = 90 - 90 * smoothstep(0, 1, rightScale);
   leftServo.write(leftRev);
   rightServo.write(rightRev);
   
-  revMsg = "Revs L" + String(leftRev) + " Right " + String(rightRev);
+  revMsg = "Revs L" + String(leftRev) + " / R" + String(rightRev);
   Serial.print(snsMsg + memMsg + revMsg + "\n");
   
   //wash out old sns data 
